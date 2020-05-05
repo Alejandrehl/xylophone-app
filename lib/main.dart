a1cd1dc9f94fec9f144129b39393d895bad7867a
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2020-05-05 10:03:43 -0400

    7 tonos agregados

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,51 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play("note1.wav");
-                },
-                child: Text("Click Me!")),
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play("note1.wav");
+                  },
+                  child: Text("Click Me 1!")),
+              FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play("note2.wav");
+                  },
+                  child: Text("Click Me 2!")),
+              FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play("note3.wav");
+                  },
+                  child: Text("Click Me 3!")),
+              FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play("note4.wav");
+                  },
+                  child: Text("Click Me 4!")),
+              FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play("note5.wav");
+                  },
+                  child: Text("Click Me 5!")),
+              FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play("note6.wav");
+                  },
+                  child: Text("Click Me 6!")),
+              FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play("note7.wav");
+                  },
+                  child: Text("Click Me 7!")),
+            ],
           ),
         ),
       ),
